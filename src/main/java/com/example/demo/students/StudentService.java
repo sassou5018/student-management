429f189d38688752a430b0eeeee164369db48996
Yacinn <sassou.est100@gmail.com>
2022-12-13 00:23:03 +0100

    added update controller and service

--- src/main/java/com/example/demo/students/StudentService.java
+++ src/main/java/com/example/demo/students/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.demo.students;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -44,5 +46,22 @@ public class StudentService {
             throw new IllegalStateException("User with id: " + studentId + " Doesn't exist");
         }
         return res;
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(
+                ()-> new IllegalStateException("Student with id: " + studentId + " Does not exist")
+        );
+        if(name!= null && name.length()>0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if(email!= null && email.length()>0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> emailCheck = studentRepository.findStudentByEmail(email);
+            if(emailCheck.isPresent()){
+                throw new IllegalStateException("Student with email: " + email + " Already exists");
+            }
+            student.setEmail(email);
+        }
     }
 }
